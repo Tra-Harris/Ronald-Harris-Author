f51f5b156d8a253b825e1565cf448faa0eb3bbb9
Trashon Harris <trashonsm@gmail.com>
2026-09-13 17:21:14 -0500

    Add contact form handling and Amazon button functionality

---
+++ script.js
@@ -0,0 +1,5 @@
+const amazonBtn = document.querySelector('.nav-btn');
+
+amazonBtn.addEventListener('click', function() {
+    window.open('https://www.amazon.com/s?k=two+masters+lucifer+wages+war&crid=394KD11ZPTTP3&sprefix=%2Caps%2C114&ref=nb_sb_ss_recent_1_0_recent', '_blank'); 
+});
